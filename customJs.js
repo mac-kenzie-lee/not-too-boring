da447dbd26cf31779c2e47189f4b0cf9ff0a39be
Mackenzie Lee <106789729+mac-kenzie-lee@users.noreply.github.com>
2022-09-30 17:01:30 -0700

    Add files via upload

---
+++ customJs.js
@@ -0,0 +1,3 @@
+document.getElementById("alertFooter").addEventListener("click", function() {
+  alert("Content, images, written and made by Mackenzie! Code includes bootstrap features. Find me at @kennehcodes on GitHub!");
+});
